393dc5df9e525abb32e62877ad948cbf455177fb
subeherachla <138214562+subeherachla@users.noreply.github.com>
2025-08-28 00:30:12 +0530

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -139,7 +139,7 @@
     $("#gender").html(p.gender);
     $("#birthdate").html(p.birthdate);
     $("#height").html(p.height);
-    $("#systolicbp").html(p.weight);
+    $("#systolicbp").html(p.systolicbp);
     $("#diastolicbp").html(p.diastolicbp);
     $("#ldl").html(p.ldl);
     $("#hdl").html(p.hdl);
